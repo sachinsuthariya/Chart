269f4b0b2b7538963a92e378502022d4d8e9bd90
Sachin Suthariya <sachinsuthariya@gmail.com>
2019-06-27 14:53:45 +0530

    added env file code refactoring

--- app.js
+++ app.js
@@ -1,62 +1,62 @@
 // import pakages
 var express = require("express"),
-    mongoose = require("mongoose"),
-    State = require("./models/state"),
-    app = express();
+  mongoose = require("mongoose"),
+  State = require("./models/state"),
+  app = express();
 
 // set view engine
 app.set("view engine", "ejs");
+require("dotenv").config();
 
 // connection url for mongoDB
 mongoose.connect("mongodb://localhost/chart", {
-    useNewUrlParser: true
+  useNewUrlParser: true
 });
 
-// Routes 
-app.get("/", function (req, res) {
-    // get chart type from from
-    var charttype;
-    if (req.query.type) {
-        charttype = req.query.type;
+// Global Variables
+const PORT = process.env.PORT || 5000;
+
+// Routes
+app.get("/", function(req, res) {
+  // get chart type from from
+  var charttype = req.query.type || "bar";
+
+  // get the data from database
+  State.find({}, function(err, stateData) {
+    if (err) {
+      res.send("Error in data fatching");
     } else {
-        charttype = "bar";
-    }
+      var stateLabels = [];
+      var statePopulation = [];
 
-    // get the data from database
-    State.find({}, function (err, stateData) {
-        if (err) {
-            res.send("Error in data fatching");
-        } else {
-            var stateLabels = [];
-            var statePopulation = [];
+      stateData.forEach(element => {
+        stateLabels.push(element.state);
+        statePopulation.push(parseInt(element.population));
+      });
 
-            stateData.forEach(element => {
-                stateLabels.push(element.state);
-                statePopulation.push(parseInt(element.population));
-            });
-
-            // created chart object 
-            var chart = {
-                type: charttype,
-                data: {
-                    labels: stateLabels,
-                    datasets: [{
-                        label: "State Population",
-                        data: statePopulation
-                    }]
-                }
-            };
-
-            // render to index.ejs
-            res.render("index", {
-                data: chart
-            });
-
+      // created chart object
+      var chart = {
+        type: charttype,
+        data: {
+          labels: stateLabels,
+          datasets: [
+            {
+              label: "State Population",
+              data: statePopulation
+            }
+          ]
         }
-    });
+      };
+
+      // render to index.ejs
+      res.render("index", {
+        data: chart
+      });
+    }
+  });
 });
 
 // port allocation
-app.listen("5000", function () {
-    console.log("Chart App Started on : localhost:5000/");
+app.listen(PORT, function() {
+  console.log("Chart App Started on : localhost:5000/");
 });
